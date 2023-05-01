ffdf72d33aea6a9aed48500db35ab37efef52c02
carliys <cflores1833@yahoo.com>
2023-05-01 16:11:31 -0700

    some changes

--- server.js
+++ server.js
@@ -1,43 +1,47 @@
-/* const express = require('express');
+const express = require('express');
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
 const PORT = process.env.port || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
+app.use(require('./routes'));
+// app.use(routes);
 
-db.once('open', () => {
-  console.log('Succefully connected to MongoDB')
-  app.listen(PORT, () => {
-    console.log(`API server forrunning on port ${PORT}!`);
-  });
-}); */
+// db.once('open', () => {
+//   console.log('Succefully connected to MongoDB')
+//   app.listen(PORT, () => {
+//     console.log(`API server forrunning on port ${PORT}!`);
+//   });
+// }); 
 
-const express = require('express');
-const routes = require('./routes');
-//const mongoose = require('mongoose');
+// const express = require('express');
+// const routes = require('./routes');
+// const mongoose = require('mongoose');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+// const app = express();
+// const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
-/*mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
-    // useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}, err => {
-    if (err) throw err;
-    console.log('Connected to MongoDB!!!')
-});*/
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// }, err => {
+//     if (err) throw err;
+//     console.log('Connected to MongoDb')
+// });
 
 // Use this to log mongo queries being executed!
 //mongoose.set('debug', true);
 
 // app.use(require('./routes'));
-app.use(routes);
+// app.use(routes);
 
-app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
+db.once("open",() => {
+  app.listen(PORT, () => { console.log(`Connected on localhost:${PORT}`)});
+});
+
+
